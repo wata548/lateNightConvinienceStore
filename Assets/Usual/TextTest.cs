6969e9dccf56fc6bea3f4865738cb9b579a64c9f
wata548 <watashimokia@gmail.com>
2024-12-27 12:52:25 +0900

    test text

--- Assets/Usual/TextTest.cs
+++ Assets/Usual/TextTest.cs
@@ -1,7 +1,8 @@
-﻿
-using System;
+﻿using System;
 using System.Collections;
+using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -43,15 +44,98 @@ public class TextTest : MonoBehaviour {
 
         text = GetComponent<TMP_Text>();
         
-        StartTyping("slk<color=#ff0000>sdjh</color>skjlsdhf");
-
+        StartTyping($"slk[{AddColor(Color.red)}sdjh{ExtractColor()}]skjlsdhf");
         
     }
 
     private float time = 0;
 
+    public void MoveText(TMP_Text text, Func<int, Vector3> math) {
+
+        text.ForceMeshUpdate();
+        var textInfo = text.textInfo;
+
+        int index = 0;
+        bool effect = false;
+        
+        foreach (var character in textInfo.characterInfo) {
+
+            if(!character.isVisible) 
+                continue;
+
+                
+
+            if (character.character == '[') {
+                effect = true;
+                continue;
+            }
+
+            if (character.character == ']' && effect) {
+                effect = false;
+            }
+            
+            
+            if (!effect) {
+                continue;
+            }
+            var vertices = textInfo.meshInfo[character.materialReferenceIndex].vertices;
+
+            var move = math(index);
+            for (int vertex = 0; vertex < 4; vertex++) {
+
+                var tempIndex = character.vertexIndex + vertex;
+                
+                var origin = vertices[tempIndex];
+                vertices[tempIndex] = origin + move;
+            }
+            index++;
+        }
+
+        foreach (var mesh in textInfo.meshInfo) {
+
+            mesh.mesh.vertices = mesh.vertices;
+        }
+
+    }
+
+    string AddColor(Color color) {
+
+        Debug.Log(color.ToString());
+        return $"<color=#{ToHex(color.r)}{ToHex(color.g)}{ToHex(color.b)}{ToHex(color.a)}>";
+
+        string ToHex(float factor) {
+
+            int value = (int)(factor * 255);
+            return Convert.ToString(value, 16).PadLeft(2, '0');
+        }
+    }
+
+    /// <summary>
+    /// Color format example: "#ff00ff" or "#ffffffdd"
+    /// </summary>
+    /// <param name="color"></param>
+    /// <returns></returns>
+    string AddColor(string color) {
+
+        return $"<color={color}>";
+    }
+
+    string ExtractColor() {
+        return "</color>";
+    }
+    
+    
     public void Update() {
 
+        if (Input.GetKeyDown(KeyCode.T)) {
+
+            int length = text.text.Length;
+            int invisible = text.textInfo.characterInfo
+                .Count(character => !character.isVisible);
+
+            Debug.Log(length - invisible);
+        }
+        
         time += Time.deltaTime;
 
         bool check = false;
@@ -63,31 +147,8 @@ public class TextTest : MonoBehaviour {
 
         if (!check)
             return;
-        
-        text.ForceMeshUpdate();
-        var textInfo = text.textInfo;
-        int j = 0;
-        foreach (var charInfo in textInfo.characterInfo) {
 
-            if(!charInfo.isVisible) 
-                continue;
-            
-            var vertexes = textInfo.meshInfo[charInfo.materialReferenceIndex].vertices;
-            
-            var randomVector =new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(-0.5f, 0.5f), 0);
-            for (int i = 0; i < 4; i++) {
-        
-                var origin = vertexes[charInfo.vertexIndex + i];
-                //vertexes[charInfo.vertexIndex + i] = origin + (Mathf.Sin(time * 10 +  (float)j /3)) * Vector3.up;
-                vertexes[charInfo.vertexIndex + i] = origin+ randomVector / 5f;
-            }
-
-            j++;
-        }
-
-        foreach (var n in textInfo.meshInfo) {
-
-            n.mesh.vertices = n.vertices;
-        }
+        MoveText(text, index => 
+            new Vector3(Random.Range(-0.3f, 0.3f), Random.Range(-0.5f, 0.5f), 0) / 5f);
     }
 }
